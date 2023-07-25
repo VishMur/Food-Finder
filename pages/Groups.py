aa71b02ba2cfa3cffe9e9280c2916e2cc5507a19
DeerEdge <vislawath.21@gmail.com>
2023-07-24 21:28:40 -0600

    retrieve docs

--- pages/Groups.py
+++ pages/Groups.py
@@ -8,15 +8,21 @@ st.set_page_config(
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("farmers").document("T6Mw2vuOJGDg4FcvK7p7")
+messages_collection = db.collection("messages")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# get all messages
+for document in messages_collection.stream():
+    st.write("Document: ", document.id)
+    st.write("Contents: ", document.to_dict())
 
-# Let's see what we got!
-st.write("The name is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# doc_ref = db.collection("messages").document("T6Mw2vuOJGDg4FcvK7p7")
+#
+# # Then get the data at that reference.
+# doc = doc_ref.get()
+#
+# # Let's see what we got!
+# st.write("The name is: ", doc.id)
+# st.write("The contents are: ", doc.to_dict())
 
 
 
