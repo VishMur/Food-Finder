ab2e96a8b6638ea9ef1d5a95a8a128e5745c6c1b
DeerEdge <vislawath.21@gmail.com>
2023-07-25 23:20:06 -0600

    this_user

--- pages/2Direct Messages.py
+++ pages/2Direct Messages.py
@@ -2,12 +2,30 @@ import streamlit as st
 from google.cloud import firestore
 from streamlit_extras.switch_page_button import switch_page
 
+this_user = "User1"
+
 st.set_page_config(
     page_title="Direct Messages",
 )
 
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-if st.button('Switch Pages'):
-    st.write("YOOOO")
-    switch_page("groups")
+# if st.button('Switch Pages'):
+#     st.write("YOOOO")
+#     switch_page("groups")
+
+messages_collection = db.collection("messages")
+st.session_state.messages = []
+
+for document in messages_collection.stream():
+    st.write("Document: ", document.id)
+    st.write("Contents: ", document.to_dict())
+    st.session_state.messages.append({"role": "user", "fromId": document.get("from"), "toId": document.get("to"), "content": document.get("msg")})
+
+for message in st.session_state.messages:
+    if message["fromId"] != this_user:
+        with st.chat_message(message["role"]):
+                st.button(message["fromId"], key=message["fromId"])
+
+if st.session_state.get('User2'):
+    st.write("caught")
