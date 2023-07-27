2dba4a798b2d59176a7b75f5d049d70c84b559a9
DeerEdge <vislawath.21@gmail.com>
2023-07-27 03:05:11 -0600

    sources 🌌

---
+++ pages/7Sources.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sources",
+)
+
+st.title("Sources")
+
+st.markdown(
+    """
+    ## Links
+    - [[1] The Problem of Food Waste](https://foodprint.org/issues/the-problem-of-food-waste/)
+    - [[2] From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [[3] Characteristics and Influential Factors of Food Deserts](https://www.ers.usda.gov/webdocs/publications/45014/30940_err140.pdf)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+
+"""
+)
+
+
