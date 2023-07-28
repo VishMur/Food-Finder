9b8380427587583484422e05922acbc40eddeb1b
DeerEdge <vislawath.21@gmail.com>
2023-07-27 22:49:14 -0600

    food deserts links

--- pages/7Sources.py
+++ pages/7Sources.py
@@ -12,7 +12,7 @@ st.markdown(
     - [[1] The Problem of Food Waste](https://foodprint.org/issues/the-problem-of-food-waste/)
     - [[2] From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
     - [[3] Characteristics and Influential Factors of Food Deserts](https://www.ers.usda.gov/webdocs/publications/45014/30940_err140.pdf)
-    - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
+    - [[4] Food Deserts and Inequality](https://www.socialpolicylab.org/post/grow-your-blog-community)
     - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
     - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
     - [From field to fork: the six stages of wasting food](https://amp.theguardian.com/environment/2016/jul/14/from-field-to-fork-the-six-stages-of-wasting-food)
