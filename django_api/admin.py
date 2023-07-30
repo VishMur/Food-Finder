9802afe85072ae7915b417c70413644e4e6f6dc5
thomaslu678 <thomaslu678@gmail.com>
2023-07-29 19:59:05 -0600

    register db models

--- django_api/admin.py
+++ django_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FoodItem, FoodType, Entity, Producer, Volunteer
 
 # Register your models here.
+
+admin.site.register(FoodItem)
+admin.site.register(FoodType)
+admin.site.register(Entity)
+admin.site.register(Producer)
+admin.site.register(Volunteer)
