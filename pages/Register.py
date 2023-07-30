e93c9ff8254c5aaeaeca969e6f0d7b92e48dcaac
thomaslu678 <thomaslu678@gmail.com>
2023-07-30 16:49:34 -0600

    clean up imports

--- pages/Register.py
+++ pages/Register.py
@@ -1,14 +1,9 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
-from django.core.exceptions import ValidationError
-from django.forms.fields import EmailField
-from django.forms.forms import Form
-from django.contrib.auth.password_validation import validate_password
-
 import streamlit as st
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 
 class CustomUserCreationForm(UserCreationForm):
