59a86cd1eb501e5a823674fe3ca2ede651b1fa9b
thomaslu678 <thomaslu678@gmail.com>
2023-07-30 19:23:44 -0600

    sample get logged in user

--- pages/Django_Logout.py
+++ pages/Django_Logout.py
@@ -1,7 +1,13 @@
 import streamlit as st
+from django_api.models import Entity, Producer
 
 st.title("Logout Page")
+current_user = st.session_state["user"]
+existing_entity = Entity.objects.filter(user=current_user).first()
+existing_producer = Producer.objects.filter(entity=existing_entity)
 
+if existing_producer.count():
+    st.markdown(existing_producer)
 
 try:
     if st.session_state["password_correct"]:
