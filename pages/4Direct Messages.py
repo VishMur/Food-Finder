7d088ab0147e0049c52c2dda943c397d80183daa
DeerEdge <vislawath.21@gmail.com>
2023-08-03 15:25:13 -0600

    chatlist ui

--- pages/4Direct Messages.py
+++ pages/4Direct Messages.py
@@ -1,10 +1,18 @@
 import streamlit as st
 from google.cloud import firestore
 from streamlit_extras.switch_page_button import switch_page
+from io import BytesIO
+import requests
+from streamlit_extras.add_vertical_space import add_vertical_space
 
 st.set_page_config(
     page_title="Direct Messages",
 )
+
+def get_image(url_string):
+    url = url_string
+    r = requests.get(url)
+    return BytesIO(r.content)
 
 if st.session_state.log == 0:
     st.header("Access Denied: Please Login First")
@@ -43,14 +51,35 @@ else:
     if st.session_state.num == "1":
         st.title("Chats")
 
-        messages_collection = db.collection("messages")
-        st.session_state.messages = []
+        users_collection = db.collection("user")
+        st.session_state.users = []
 
-        for document in messages_collection.stream():
-            st.session_state.messages.append({"role": "user", "fromId": document.get("from"), "toId": document.get("to"),
-                                              "content": document.get("msg")})
-        for message in st.session_state.messages:
-            if message["fromId"] != this_user:
-                with st.chat_message(message["role"]):
-                    st.button(message["fromId"], key=message["fromId"], on_click=route_to_chat_view, args=[message["fromId"]])
+        for document in users_collection.stream():
+            st.session_state.users.append({"role": "user", "name": document.get("name"),
+                                           "profile_img": document.get("profile_img"),
+                                           "join_date": document.get("join_date"),
+                                           "user_type": document.get("user_type"),})
+        for user in st.session_state.users:
+            if user["name"] != this_user:
+                with st.chat_message(user["role"], avatar="🗞"):
+                    col1, col2, col3, col4 = st.columns(4)
+
+                    with col1:
+                        st.image(get_image(user["profile_img"]), width=100)
+
+                    with col2:
+                        st.header(user["name"])
+                        st.write(user["user_type"])
+
+                    with col3:
+                        st.write("")
+                    with col4:
+                        add_vertical_space(1)
+                        st.button("Enter Chat", key=user["name"], on_click=route_to_chat_view, args=[user["name"]])
+
+                        st.write(user["join_date"])
+
+
+
+
 
