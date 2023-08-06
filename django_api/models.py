bbe8d3e6e0f1df1c5360ac95d658f198117d4a20
thomaslu678 <thomaslu678@gmail.com>
2023-08-05 20:28:14 -0600

    create farm-related models

--- django_api/models.py
+++ django_api/models.py
@@ -37,6 +37,17 @@ class Producer(models.Model):
     def __str__(self):
         return self.entity.__str__()
 
+class Farm(models.Model):
+    entity = models.OneToOneField(Entity, on_delete=models.CASCADE)
+    description = models.TextField()
+    deliveries = models.IntegerField(default=0)
+    usda_certified = models.BooleanField(null=True)
+    image_link = models.TextField(blank=True)
+    website_link = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.entity.__str__()
+
 class Volunteer(models.Model):
     entity = models.OneToOneField(Entity, on_delete=models.CASCADE)
     deliveries = models.IntegerField(default=0)
@@ -51,6 +62,13 @@ class ProducerBookmark(models.Model):
     class Meta:
         unique_together = ["producer", "volunteer"]
 
+class FarmBookmark(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.PROTECT)
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.PROTECT)
+    date_bookmarked = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ["farm", "volunteer"]
+
 class FoodItem(models.Model):
     name = models.CharField(max_length=100)
     type = models.ForeignKey(FoodType, on_delete=models.CASCADE)
@@ -63,4 +81,14 @@ class FoodItem(models.Model):
     def __str__(self):
         return self.producer.__str__() + ": " + self.name
 
+class FarmItem(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.ForeignKey(FoodType, on_delete=models.CASCADE)
+    description = models.TextField()
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=True, blank=True)
+    consumable = models.BooleanField(null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.farm.__str__() + ": " + self.name
